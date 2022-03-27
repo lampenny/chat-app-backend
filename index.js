eaf4d55332bb604ed20056660d41be0a7b7dc280
Penny Lam <pennylam@live.com>
2022-03-28 08:15:08 +0900

    update env

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 app.get('/', (req, res) => {
-    res.send('Hello, world');
+    res.send('Hello, world its working');
 })
 
 app.use('/auth', authRoutes);
